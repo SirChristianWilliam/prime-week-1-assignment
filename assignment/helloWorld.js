709ce929ce3b01a24ca2f6024696b9ce1bd3be1c
Chris Maki <c.william.developer@gmail.com>
2022-08-16 20:07:25 -0500

    Added some javascript to top it off

---
+++ assignment/helloWorld.js
@@ -0,0 +1,19 @@
+function hideIt(event) {
+    document.getElementById("btn").style.display = "none";
+      if(confirm("If you like this webpage, select 'Ok'!") == true) {
+        return good();
+      } else{
+        return notGood();
+      }
+      }
+
+      function notGood() {
+        document.getElementById("hiddenBad").style.visibility = "visible";
+      }
+
+      function good() {
+        document.getElementById("hiddenGood").style.visibility = "visible";
+      }
+    
+
+ 
